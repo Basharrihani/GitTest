8210c936ba8de87c304c6b66a2a19c75c3fb1298
Bashar Al-Rihani <basharnalrihani@gmail.com>
2021-10-29 20:50:48 +0200

    adding the files

---
+++ src/MyAdder.java
@@ -0,0 +1,6 @@
+public class MyAdder {
+    public static double sum(double x, double y)
+    {
+        return x+y;
+    }
+}
---
+++ src/Testing.java
@@ -0,0 +1,8 @@
+public class Testing {
+
+    public static void main(String[] args) {
+        double x = 5;
+        double z = 3;
+        System.out.println(MyAdder.sum(x,z));
+    }
+}
