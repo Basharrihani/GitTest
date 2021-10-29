89401cd5c192724509a26f809e7d69b0c01834d1
Bashar Al-Rihani <basharnalrihani@gmail.com>
2021-10-29 20:55:19 +0200

    editing the files

--- src/Testing.java
+++ src/Testing.java
@@ -4,5 +4,9 @@ public class Testing {
         double x = 5;
         double z = 3;
         System.out.println(MyAdder.sum(x,z));
+
+        x = 7;
+        z = 7;
+        System.out.println(MyAdder.sum(x,z));
     }
 }
